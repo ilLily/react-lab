667db9fbcee7376fa3a99bf64d2987066cfafdd7
Lily Kuo <ilwitulily0209@gmail.com>
2023-06-21 11:21:55 +0800

    menu

---
+++ pages/course/menu.js
@@ -0,0 +1,20 @@
+function Menu() {
+  // [active,setActive]={'active'}
+  return (
+    <>
+      <ul>
+        <li>
+          <a>首頁</a>
+        </li>
+        <li className={'active'}>
+          <a>關於我們</a>
+        </li>
+        <li>
+          <a>產品</a>
+        </li>
+      </ul>
+    </>
+  );
+}
+
+export default Menu;
