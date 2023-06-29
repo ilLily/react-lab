b8fd228aff25c20663fe4d7e7c7dfca306d5b463
Lily Kuo <ilwitulily0209@gmail.com>
2023-06-29 17:47:29 +0800

    icon

--- pages/antd/testGrid.js
+++ pages/antd/testGrid.js
@@ -15,49 +15,66 @@ export default function TestGrid() {
               </div>
             </div>
           </Space>
-
-          <div className="Lily cardbox">
-            <Link href="/antd/testBtn">
-              <div className="card">
-                <img src="/testImg/dog-girl-hugging.jpg" alt="" />
-                <h5>My Activity</h5>
-                <p>2023-05-04</p>
-              </div>
-            </Link>
-            <div className="card">
+        </div>
+        <div className="container-inner">
+          <div className="card2">
+            <div className="card2-img">
               <img src="/testImg/dog-girl-hugging.jpg" alt="" />
-              <h5>My Activity</h5>
-              <p>2023-05-04</p>
             </div>
-            <div className="card">
-              <img src="/testImg/dog-girl-hugging.jpg" alt="" />
-              <h5>My Activity</h5>
-              <p>2023-05-04</p>
+
+            <div className="card2-body">
+              <p className="productname">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam
+              </p>
+              <div className="icons">
+                <div>icon</div>
+                <div>icon</div>
+              </div>
+              <div className="icons">
+                <p>hello</p>
+                <p>hello</p>
+              </div>
             </div>
           </div>
-
-          <h1>3 cards</h1>
-          <Row style={{ border: '2px solid pink' }}>
-            <Col
-              style={{ border: '2px solid orange', height: '200px' }}
-              span={8}
-            >
-              col-8
-            </Col>
-            <Col
-              style={{ border: '2px solid orange', height: '200px' }}
-              span={8}
-            >
-              col-8
-            </Col>
-            <Col
-              style={{ border: '2px solid orange', height: '200px' }}
-              span={8}
-            >
-              col-8
-            </Col>
-          </Row>
         </div>
+        <div className="container-inner">
+          <Link href="/antd/testBtn">
+            <div className="card">
+              <img src="" alt="" />
+              <h5>My Activity</h5>
+              <p>2023-05-04</p>
+            </div>
+          </Link>
+          <div className="card">
+            <img src="/testImg/dog-girl-hugging.jpg" alt="" />
+            <h5>My Activity</h5>
+            <p>2023-05-04</p>
+          </div>
+          <div className="card">
+            <img src="/testImg/dog-girl-hugging.jpg" alt="" />
+            <h5>My Activity</h5>
+            <p>2023-05-04</p>
+          </div>
+        </div>
+      </div>
+      <div className="container-inner">
+        <div className="card">
+          <img src="" alt="" />
+        </div>
+      </div>
+      <div className="container-outer">
+        <h1>3 cards</h1>
+        <Row>
+          <Col style={{ border: '2px solid orange', height: '200px' }} span={8}>
+            col-8
+          </Col>
+          <Col style={{ border: '2px solid orange', height: '200px' }} span={8}>
+            col-8
+          </Col>
+          <Col style={{ border: '2px solid orange', height: '200px' }} span={8}>
+            col-8
+          </Col>
+        </Row>
       </div>
     </>
   );
